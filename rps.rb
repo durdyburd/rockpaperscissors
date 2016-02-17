fa00af5d4bb40d2d5f9d418c1e610a70f08f14d7
William Durden <williamdurden@Williams-MacBook-Air.local>
2016-02-17 13:39:06 -0600

    working

--- rps.rb
+++ rps.rb
@@ -1,71 +1,55 @@
-# starting line
-puts "Do you wish to play a game, human?"
-puts "y/n"
-user_choice = gets.chomp
-if user_choice == "y"
-  puts "Welcome to Rock, Paper, Scissors silly human."
-else
-  puts "Perhaps another time."
-end
-
-
 class Game
-  choices = %w[rock, paper, scissors]
-  rules = []
 
   attr_accessor :player_one_choice, :computer
 
-  def initialize(player_one_choice, computer)
-    self.player_one_choice = player_one_choice.gets.chomp
+  def initialize
+    self.player_one_choice = player_one_choice
     self.computer = computer
-#define rules for game
 
-#gets player input
+  end
 
-    def player_one_choice |choice|
-      puts "Choose your fate:"
-      answer = gets.chomp
-      if
-        answer ==
-
-#generates random computer response
-
-#decides which choice is winner,
-#tells player whether they won or not,
-#asks if player wants to play again
+  def play
+    puts "Choose rock, paper, or scissors"
+    player_one_choice = gets.chomp
+    choices = ["rock", "paper", "scissors"]
+    computer = choices.sample
 
 
-end
+    if player_one_choice == "rock" && computer == "scissors"
+      print "Player chose #{player_one_choice}, computer chose #{computer}."
+      print "Rock beats scissors, player wins."
+      puts
+
+    elsif player_one_choice == "scissors" && computer == "paper"
+      print "Player chose #{player_one_choice}, computer chose #{computer}."
+      print "Scissors beats paper, player wins"
+      puts
+
+    elsif player_one_choice == "paper" && computer == "rock"
+      print "Player chose #{player_one_choice}, computer chose #{computer}"
+      print "Paper beats rock, player wins"
+      puts
+
+    elsif computer == "rock" && player_one_choice == "scissors"
+      print "Player chose #{player_one_choice}, computer chose #{computer}."
+      print "Rock beats scissors, computer wins"
+      puts
+
+    elsif computer == "scissors" && player_one_choice == "paper"
+      print "Player chose #{player_one_choice}, computer chose #{computer}."
+      print "Scissors beats paper, computer wins"
+      puts
+
+    elsif computer == "paper" && player_one_choice == "rock"
+      print "Player chose #{player_one_choice}, computer chose #{computer}."
+      print "Paper beats rock, computer wins"
+      puts
+
+      end
+    end
+
 
 game = Game.new
 game.play
 
-player_one_choice = gets.chomp
-
-#p for paper
-#s for scissors
-#r for rock
-
-#if player_input == rock and computer_input == scissors
-    #puts "Player chose rock; computer chose scissors."
-    #puts "Rock beats scissors player wins."
-
-  #elsif player_input == scissors and computer_input == paper
-    #puts "Player chose scissors; computer chose paper."
-    #puts "Scissors beats paper, player wins"
-
-  #elsif player_input == paper and computer_input == rock
-    #puts "Player chose paper; computer chose rock"
-    #puts "Paper beats rock, player wins"
-
-  #elsif computer_input == rock and player_input == scissors
-    #puts "Player chose scissors; computer chose rock."
-    #puts "Rock beats scissors, computer wins"
-
-  #elsif computer_input == scissors and player_input == paper
-    #puts "Player chose paper. computer chose scissors."
-    #puts "Scissors beats paper, computer wins"
-
-  #elsif computer_input == paper and player_input == rock
-    #puts "Player chose rock. computer chose paper."
-    #puts "Paper beats rock, computer wins"
+end
